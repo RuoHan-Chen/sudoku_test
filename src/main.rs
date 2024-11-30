fd65aaaa02aacf9e07fef2282de0e46d8efcffa3
Jason Lee <jaeho52lee@gmail.com>
2024-11-29 19:00:06 -0600

    reading filepath

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{self, Write}; 
+
 fn main() {
-    println!("Hello, world!");
+    print!("Enter path to puzzle file: "); 
+    io::stdout().flush().unwrap(); 
+
+    let mut input = String::new(); 
+    io::stdin().read_line(&mut input).unwrap();
+
+    let defaultFile = "sudoku-test1.txt"; 
+    let fileSuffix = if input.trim().is_empty() {
+        defaultFile
+    } else {
+        input.trim() 
+    }; 
+    let filename = &("../txt/".to_owned() + fileSuffix); 
+
+    println!("Using file: {}", filename); 
 }
